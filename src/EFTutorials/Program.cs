718c40d9cbeae8d6f9748bf2537fab37bcfc5394
kareemaismail <kareema.ismail@gmail.com>
2015-07-16 09:39:25 +0200

    Added console commands to experiment with db retrieval.
    Student.Courses is populated, but Student.Standard is null.

--- src/EFTutorials/Program.cs
+++ src/EFTutorials/Program.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Entity.Core.Objects;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using EFTutorials;
 
 namespace EFBasicTutorials
 {
@@ -10,6 +12,18 @@ namespace EFBasicTutorials
     {
         static void Main(string[] args)
         {
+            using (SchoolDBEntities context = new SchoolDBEntities())
+            {
+                Student studentEntity = context.Students.FirstOrDefault<Student>();
+                var entityType = ObjectContext.GetObjectType(studentEntity.GetType());
+
+
+                Console.WriteLine("Student Entity Type: " + entityType);
+                Console.WriteLine("Student Standard: " + studentEntity.Standard.StandardName);
+            }
+
+            Console.WriteLine("Press any key to exit.");
+            Console.ReadKey();
         }
     }
 }
