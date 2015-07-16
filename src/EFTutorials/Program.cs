ae1b375bca9d46f44d32fab51a37e67f57e4575d
kareemaismail <kareema.ismail@gmail.com>
2015-07-16 09:42:24 +0200

    Made the solution compile.

--- src/EFTutorials/Program.cs
+++ src/EFTutorials/Program.cs
@@ -4,24 +4,13 @@ using System.Data.Entity.Core.Objects;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using EFTutorials;
 
-namespace EFBasicTutorials
+namespace EFTutorials
 {
     class Program
     {
         static void Main(string[] args)
         {
-            using (SchoolDBEntities context = new SchoolDBEntities())
-            {
-                Student studentEntity = context.Students.FirstOrDefault<Student>();
-                var entityType = ObjectContext.GetObjectType(studentEntity.GetType());
-
-
-                Console.WriteLine("Student Entity Type: " + entityType);
-                Console.WriteLine("Student Standard: " + studentEntity.Standard.StandardName);
-            }
-
             Console.WriteLine("Press any key to exit.");
             Console.ReadKey();
         }
